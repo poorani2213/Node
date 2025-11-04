ad6fbce2c4e813f4fb5e5957184cd098b4ddb0fd
poorani2213 <pooranikumar2004@gmail.com>
2025-11-04 12:41:11 +0530

    Initial commit for backend project

---
+++ main.js
@@ -0,0 +1,5 @@
+import { add, multiply , sub} from "./math.js";
+
+console.log("Addition:", add(5, 3));
+console.log("subraction",sub(10,4));
+console.log("Multiplication:", multiply(4, 2));
---
+++ math.js
@@ -0,0 +1,18 @@
+let add = (a,b) =>{
+
+  return a+b
+
+}
+
+export  let sub = (a,b) =>{
+
+    return a-b
+}
+
+export let multiply = (a,b) =>{
+
+    return a*b
+
+}
+
+export {add}
